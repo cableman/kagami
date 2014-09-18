86653ba7a441f023b4253f53378b321ae05d86e5
Jesper Kristensen <cableman@linuxdev.dk>
2014-09-18 16:59:15 +0200

    Added missing documentation to dummy plugin

--- plugins/dummy_content/dummy_content.js
+++ plugins/dummy_content/dummy_content.js
@@ -7,14 +7,20 @@
  * Register the plugin with architect.
  */
 module.exports = function (options, imports, register) {
+  // Connect to the event bus.
   var kagami = imports.kagami;
 
+  // Listen to all request view events.
   kagami.on('request-view', function(data) {
+    // Return test data in response to view request.
     kagami.emit('response-view', {
       'region_id': data.region_id,
       'view': '<h3>Region <em>' + data.region_id + '</em></h3>'
     });
   });
 
+  /**
+   * Register the plugin with architect.
+   */
   register(null, null);
 }
